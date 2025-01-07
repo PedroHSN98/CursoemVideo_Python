4bb51819b98d4ff339761b732bf83f2423c2b61e
Pedro Henrique Santana Nascimento <pedrohenriquesn90@hotmail.com>
2025-01-07 08:39:22 -0400

    at

--- Aula/aula08.py
+++ Aula/aula08.py
@@ -1,6 +1,7 @@
 from math import sqrt
-#import math
+import math
 
 num = int(input('Digite um número: '))
 raiz = math.sqrt(num)
-print(f'A raiz de {num} é igual a {raiz}')
+print(f'A raiz de {num} é igual a {raiz:.2f29
+}')
