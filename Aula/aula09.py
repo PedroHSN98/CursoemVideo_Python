cb3812a769c32d7faa6430343e2268c44841ac7f
Pedro Henrique Santana Nascimento <pedrohenriquesn90@hotmail.com>
2025-01-08 08:34:35 -0400

    Create aula09.py

---
+++ Aula/aula09.py
@@ -0,0 +1,13 @@
+frase = 'Curso em Vídeo Python'
+print(frase[::2])
+#print(frase.count('o'))
+#print(frase.upper().count('o'))
+#print(len(frase))
+
+
+
+
+#print('''asdjijasd nasijasdjiaijasmiasmiasjpoiaajoiaiajipasopasd
+#     asdnuashuajoidasjoiasjiasoijadsmjiasd
+#      aoih3i231noi32nio23noi23joi324323423
+#      23oij32noiewnoiewrnjoiewnoiewrmiew''')
