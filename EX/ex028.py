346c843bef4acb3276c37665e5b48864914e5143
Pedro Henrique Santana Nascimento <pedrohenriquesn90@hotmail.com>
2025-01-14 07:55:04 -0400

    game

--- EX/ex028.py
+++ EX/ex028.py
@@ -1,10 +1,13 @@
-from random import randint 
-computador = randint(0, 5) # Faz o computador "PENSAR"
-print('=' * 20)
-print('Vou pensar em um número entre 0 e 5. Tente adivinhasr....')
-print('=' * 20)
-jogador = int(input('Em que número eu pensei? '))# Jogador tenta adivinhar
+from random import randint
+from time import sleep
+computador = randint(0, 5) # Faz o computador "Pensar"
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20)
+jogador = int(input('Emque número eu pensei? ')) #Jogador tenta adivinhar
+print('PROCESSANDO...')
+sleep(3)
 if jogador == computador:
-    print('Parabéns! Você conseguiu me verncer! ')
+    print('Parabéns! Você conseguiu me vencer!')
 else:
-    print(f'Ganhei! Eu pensei no número {computador} e não no {jogador}')
+    print(f'Ganhei! Eu pensei no número {computador} e não no {jogador}!')
