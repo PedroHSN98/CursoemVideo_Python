34259140b1cfc2eabc1be4f9030c20fb673ec8c3
Pedro Henrique Santana Nascimento <pedrohenriquesn90@hotmail.com>
2025-01-27 08:12:53 -0400

    feito

--- Ex Mundo2/ex049.py
+++ Ex Mundo2/ex049.py
@@ -3,5 +3,5 @@ Refaça o desafio 009, mostrando a tabuada de um numero que o usuario escolher, 
 '''
 
 num = int(input('Digite um número para ver a sua tabuada: '))
-for i in range(0, 11):
-    print(f'{num * i}')
+for i in range(1, 11):
+    print(f'{num} x {i} = {num * i}')
