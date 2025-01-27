6a0b08452d6ddfaa7da91babbec05cc5336b508a
Pedro Henrique Santana Nascimento <pedrohenriquesn90@hotmail.com>
2025-01-27 09:32:38 -0400

    .

--- Ex Mundo2/ex050.py
+++ Ex Mundo2/ex050.py
@@ -2,11 +2,9 @@
 Desenvolva um programa que leia seis numeros inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for Impar. Desconsidere
 '''
 soma = 0
-for i in range(0, 6):
-    n = int(input('Digite 6 numeros inteiros para serem somados: '))
-    if n % 2 == 0:
-        soma += n
-    else:
-        print(f'{n} é ímpar e foi desconsiderado.')
-
-print(f'A soma dos números pares digitados é: {soma}')
+cont = 0 
+for i in range(1, 7):
+    num = int(input(f'Digite o {i} valor: '))
+    soma += num
+    cont += 1
+print(f'Você informou {cont} números e a soma foi {soma}')
