10d3623bb1c078b8d7caba39b357be1af20f3b6a
Pedro Henrique Santana Nascimento <pedrohenriquesn90@hotmail.com>
2025-01-27 07:56:49 -0400

    or

--- Ex Mundo2/ex047.py
+++ Ex Mundo2/ex047.py
@@ -6,3 +6,9 @@ for i in range(1, 51):
     if i % 2 == 0:
         print(i)
 print(f'Esses numeros são divisiveis por 2!')
+
+# or 
+
+#for i in range (2, 51, 2):
+    #print(n, end= ' ')
+#print('Acabou')
