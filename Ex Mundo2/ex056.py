1721aaae48965f1d3384dfac1c33e8e7d1fcf6a1
Pedro Henrique Santana Nascimento <pedrohenriquesn90@hotmail.com>
2025-01-28 09:37:27 -0400

    56

--- Ex Mundo2/ex056.py
+++ Ex Mundo2/ex056.py
@@ -2,6 +2,11 @@
 
 somaidade = 0
 mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
+
+
 for p in range(1, 5):
     print(f'---{p}----')
     nome = str(input('Nome: ')).strip()
@@ -11,5 +16,11 @@ for p in range(1, 5):
     if p == 1 and sexo in 'Mm':
         maioridadehomem = idade
         nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20: 
+        totmulher20 += 1
 mediaidade += somaidade/ 4
 print(f'A média de idade do grupo é de {mediaidade} anos')
+print(f'O homem mais velho tem {maioridadehomem} anos e se chaa {nomevelho}')
